7ffe7ffa93b83be97a79929c1092df9c2d538ff6
Elijah Kabatsi <e.kabatsi@alustudent.com>
2026-04-01 06:19:14 +0000

    Fix Nginx ULIMIT with exact sed match

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# Fix Nginx to handle more requests by increasing the ULIMIT
+# Fix Nginx to handle more requests by increasing the ULIMIT open files
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && nginx -s reload',
-  path    => '/usr/local/bin/:/bin/:/usr/sbin/',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && service nginx restart',
+  path    => '/usr/local/bin/:/bin/:/usr/sbin/:/usr/bin/',
 }
